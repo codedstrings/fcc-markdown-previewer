9aab9ea9c1811cf9c17a34cd6e4f2af95b3d333e
codedstrings <mridhulml@gmail.com>
2025-08-14 07:21:40 +0530

    feat: basic working prototype
    - implement markdown preiview with marked library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // State to hold the markdown input
+  // const [markdown, setMarkdown] = useState('');
+
+const [markdownText, setMarkdownText] = useState('');
+
+const handleInput = (event) => {
+  const input = event.target.value;
+  setMarkdownText(input); // Store the raw markdown
+}
+
+// Convert to HTML in the render
+const htmlOutput = marked.parse(markdownText);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Markdown Previewer</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <label htmlFor="editor"></label>
+        <textarea
+          id="editor"
+          onChange={handleInput}
+          placeholder="Enter Markdown here"
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="preview" dangerouslySetInnerHTML={{__html:htmlOutput}}></div>
     </>
-  )
+  );
 }
 
 export default App
