0604b5cd91d562dd661a1a7e843a1c7c25b0fa8b
codedstrings <mridhulml@gmail.com>
2025-08-14 10:30:15 +0530

    ref: remove unecessary comments and format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css'
-import { useState } from 'react';
-import { marked } from 'marked';
+import "./App.css";
+import { useState } from "react";
+import { marked } from "marked";
 
 function App() {
-
   // State to hold the markdown input
-  // const [markdown, setMarkdown] = useState('');
+  const [markdownText, setMarkdownText] = useState("");
 
-const [markdownText, setMarkdownText] = useState('');
+  const handleInput = (event) => {
+    const input = event.target.value;
+    setMarkdownText(input); // Store the raw markdown
+  };
 
-const handleInput = (event) => {
-  const input = event.target.value;
-  setMarkdownText(input); // Store the raw markdown
-}
-
-// Convert to HTML in the render
-const htmlOutput = marked.parse(markdownText);
+  // Convert to HTML in the render
+  const htmlOutput = marked.parse(markdownText);
 
   return (
     <>
@@ -28,9 +25,9 @@ const htmlOutput = marked.parse(markdownText);
           placeholder="Enter Markdown here"
         />
       </div>
-      <div id="preview" dangerouslySetInnerHTML={{__html:htmlOutput}}></div>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: htmlOutput }}></div>
     </>
   );
 }
 
-export default App
+export default App;
